541610c21df6312e9f8a49043a883e7f5bb644f4
Liandra Whittaker <liawhitt200@gmail.com>
2024-02-15 23:05:08 -0500

    most of lab done

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -17,6 +17,13 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+def format_date_Joined(day, month, year):
+    return month, year
 
 ###
 # The functions below should be applicable to all Flask apps.
